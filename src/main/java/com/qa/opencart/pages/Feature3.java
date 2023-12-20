dafe8ceb143c732df371698f5609721ea070d30c
Yuonus <syuonus@gmail.com>
2023-12-20 17:51:21 -0500

    Feature 3 is ready to be released.

---
+++ src/main/java/com/qa/opencart/pages/Feature3.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class Feature3 {
+
+	public static void main(String[] args) {
+		System.out.println("S_Tech will make u the real automation tester");
+		System.out.println("We proud to be part of the S_Tech family.");
+	}
+
+}
