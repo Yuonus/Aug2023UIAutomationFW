16a83d222e54487e84a43bace5d8146cd6c7247f
Yuonus <syuonus@gmail.com>
2023-12-20 17:45:17 -0500

    Feature 2 is ready

---
+++ src/main/java/com/qa/opencart/pages/Feature2.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class Feature2 {
+	public static void main(String[] args) {
+		System.out.println("S_Tech will really make you professional in automation testing.");
+		
+		String bestSDETSchool = "S_Tech is the best SDET school in USA.";
+	}
+
+}
